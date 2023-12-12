0ff2d3367835213a8b9912b8a269155f93f4fb62
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-12-12 22:23:24 +0530

    Instructor

--- byte-learn-client/src/component/courses/Instructors.js
+++ byte-learn-client/src/component/courses/Instructors.js
@@ -37,8 +37,8 @@ const Instructor = () => {
               </div>
               <div className="text-column">
                 <h2>Rahul Kumar</h2>
-                <h5>CS @Adobe, Instructor @Code-Help</h5>
-                <p>Rahul Kumar is an ace software engineer working in the role of <strong>Computer Scientist</strong> at <strong>Adobe Systems</strong> and a popular computer science instructor on <strong>CodeHelp</strong> Youtube.He is working in the industry for the past <strong>4.5 years</strong>, working on different real-world problems. He is well-known among students for his amazingly simplified explanations with real-life examples, enabling students to understand complex topics very easily. Many of his ex-students are now working in top product companies like <strong>Microsoft, Amazon, De-Shaw</strong> etc.</p>
+                <h5>CS @Adobe, Instructor @Byte-Learn</h5>
+                <p>Rahul Kumar is an ace software engineer working in the role of <strong>Computer Scientist</strong> at <strong>Adobe Systems</strong> and a popular computer science instructor on <strong>ByteLearn</strong> Youtube.He is working in the industry for the past <strong>4.5 years</strong>, working on different real-world problems. He is well-known among students for his amazingly simplified explanations with real-life examples, enabling students to understand complex topics very easily. Many of his ex-students are now working in top product companies like <strong>Microsoft, Amazon, De-Shaw</strong> etc.</p>
               </div>
             </div>
           </div>
@@ -51,7 +51,7 @@ const Instructor = () => {
               </div>
               <div className="text-column">
                 <h2>Sonali Singh</h2>
-                <h5>Founder - Code-Help, Ex-Amazon, Ex-Microsoft</h5>
+                <h5>Founder - ByteLearn, Ex-Amazon, Ex-Microsoft</h5>
                 <p>Sonali Singh is a <strong>Software Engineer</strong> and a Youtuber, primarily known for his Coding and Software Engineering skills. He is quite a popular figure among students as well as working professionals on various social media platforms, his YouTube and LinkedIn profiles amassing almost <strong>1M subscribers</strong>. He's also mentored <strong>500k+ students</strong> so far. He has done his B.Tech (IT) from the Netaji Subhash Institute of Technology (NSIT), Delhi, and worked in <strong>Amazon</strong> and <strong>Microsoft</strong>.</p>
               </div>
             </div>
