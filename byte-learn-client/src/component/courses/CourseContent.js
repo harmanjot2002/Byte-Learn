7d14f489d06386964bfdd54f2c082fad24b19916
aftab1911 <aftab0103.be21@chitkara.edu.in>
2023-12-16 18:54:56 +0530

    AddedContent

--- byte-learn-client/src/component/courses/CourseContent.js
+++ byte-learn-client/src/component/courses/CourseContent.js
@@ -1,11 +1,164 @@
-import React from 'react'
+import React,{useState} from 'react';
+import './CourseContent.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 const CourseContent = () => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  // for second 
+
+  const [isDropdownVisible2, setDropdownVisible2] = useState(false);
+
+  const toggleDropdown2 = () => {
+    setDropdownVisible2(!isDropdownVisible2);
+  };
+
+  // for third 
+  const [isDropdownVisible3, setDropdownVisible3] = useState(false);
+
+  const toggleDropdown3 = () => {
+    setDropdownVisible3(!isDropdownVisible3);
+  };
+
+  //for fourth
+  const [isDropdownVisible4, setDropdownVisible4] = useState(false);
+
+  const toggleDropdown4 = () => {
+    setDropdownVisible4(!isDropdownVisible4);
+  };
+
+  // for fifth
+  const [isDropdownVisible5, setDropdownVisible5] = useState(false);
+
+  const toggleDropdown5 = () => {
+    setDropdownVisible5(!isDropdownVisible5);
+  };
+
+   // for sixth 
+
+   const [isDropdownVisible6, setDropdownVisible6] = useState(false);
+
+   const toggleDropdown6 = () => {
+     setDropdownVisible6(!isDropdownVisible6);
+   };
+
+
   return (
-    <div id="coursecontent">
-      course content
-    </div>
-  )
+    <>
+      <div className='contentouter'>
+        <div className='tocenter'>
+          <hr style={{ width: "100%", size: "20" }} />
+          <h2 className='content-heading'>Course Content</h2>
+          <div className='contentpara' onClick={toggleDropdown}>
+            <p className='drop-para'>HTML Basics</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible ? 'show' : ''}`}>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Setting up tools, Introduction to Web Development, Basics of HTML</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Understanding and Handling Forms and Input</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Selectors and Introduction to Styling, Different types of CSS</p>
+          </div>
+
+          {/* Second dropdownpata */}
+
+          <div className='contentpara' onClick={toggleDropdown2}>
+            <p className='drop-para'>CSS Basics</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible2 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible2 ? 'show' : ''}`}>
+            <br />
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Selectors, Box Model, Styling Fonts and Images</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Learn About Height-Width</p>
+          </div>
+
+          {/* Third Downpara */}
+          <div className='contentpara' onClick={toggleDropdown3}>
+            <p className='drop-para'>Flex Box and Grid</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible3 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible3 ? 'show' : ''}`}>
+            <br />
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>CSS Flexbox, Responsiveness, Grid Container,</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Minor Project based on HTML and CSS</p>
+          </div>
+
+          {/* Fourth Downpara */}
+
+          <div className='contentpara' onClick={toggleDropdown4}>
+            <p className='drop-para'>Animation and Responsiveness</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible4 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible4 ? 'show' : ''}`}>
+        
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Animation and keyframes , Transition, Shorthand Animation</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Media Query, Responsive Design of Text and Images</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Responsive Mini Project</p>
+          </div>
+
+           {/* Fifth para  */}
+
+           <div className='contentpara' onClick={toggleDropdown5}>
+            <p className='drop-para'>JavaScript Basics</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible5 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible5 ? 'show' : ''}`}>
+        
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Introduction to JS, Variable, Datatypes,Operator Precidence</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Control Statements if else, loops, Switch Case, Array and Strings</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Scope Chaining, this keyword, Functions, Callback, Arrow Functions</p>
+          </div>
+
+            {/* Sixth para */}
+            <div className='contentpara' onClick={toggleDropdown6}>
+            <p className='drop-para'>DOM Manipulation and Modern JS</p>
+            <img
+              className={`dropdown-img ${isDropdownVisible6 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downpara ${isDropdownVisible6 ? 'show' : ''}`}>
+        
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>DOM, BOM, Window Object, Mini Project</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Async-Await, Event Loop, Event Listners</p>
+            <p className='downn'><i className="fa fa-video-camera" aria-hidden="true"></i>Classes and Exports, Getters and Setters, Promises, Fetch</p>
+          </div>
+          <hr style={{ width: "100%", size: "20",marginTop:"10%" }} />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CourseContent
+export default CourseContent;
+
--- byte-learn-client/src/component/courses/FAQs.js
+++ byte-learn-client/src/component/courses/FAQs.js
@@ -1,11 +1,109 @@
-import React from 'react'
+import React,{useState} from 'react';
+import './FAQs.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 const FAQs = () => {
+  const [isDropdownVisible10, setDropdownVisible10] = useState(false);
+
+  const toggleDropdown10 = () => {
+    setDropdownVisible10(!isDropdownVisible10);
+  };
+
+  // for second 
+
+  const [isDropdownVisible11, setDropdownVisible11] = useState(false);
+
+  const toggleDropdown11 = () => {
+    setDropdownVisible11(!isDropdownVisible11);
+  };
+
+  // for third 
+  const [isDropdownVisible12, setDropdownVisible12] = useState(false);
+
+  const toggleDropdown12 = () => {
+    setDropdownVisible12(!isDropdownVisible12);
+  };
+
+  //for fourth
+  const [isDropdownVisible13, setDropdownVisible13] = useState(false);
+
+  const toggleDropdown13 = () => {
+    setDropdownVisible13(!isDropdownVisible13);
+  };
+
+
   return (
-    <div id="faqs">
-      FAQs
-    </div>
-  )
+    <>
+      <div className='contentouterF'>
+        <div className='tocenterF'>
+          <hr style={{ width: "100%", size: "20" }} />
+          <h2 className='content-headingF'>FAQs</h2>
+          <div className='contentparaF' onClick={toggleDropdown10}>
+            <p className='drop-paraF'>I don't have any Prior experience of any Programing Language Can I take this Course?</p>
+            <img
+              className={`dropdown-imgF ${isDropdownVisible10 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downparaF ${isDropdownVisible10 ? 'show' : ''}`}>
+            <p className='downnF'>Yes, This Course does not require any pre requisite you can startoff with this Course</p>
+          </div>
+
+          {/* Second dropdownpata */}
+
+          <div className='contentparaF' onClick={toggleDropdown11}>
+            <p className='drop-paraF'>Will I get the Recorded Access of this Course?</p>
+            <img
+              className={`dropdown-imgF ${isDropdownVisible11 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downparaF ${isDropdownVisible11 ? 'show' : ''}`}>
+            <p className='downnF'>Yes a Recording of 1 Year will be Provided after Completion of the Course</p>
+          </div>
+
+
+          {/* Third Downpara */}
+          <div className='contentparaF' onClick={toggleDropdown12}>
+            <p className='drop-paraF'>Do I need to Learn DSA before Learning Web Development</p>
+            <img
+              className={`dropdown-imgF ${isDropdownVisible12 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downparaF ${isDropdownVisible12 ? 'show' : ''}`}>
+        
+            <p className='downnF'>No You don't need any prior Knowledge of DSA or any Programming Language Before Enrolling into this Course</p>
+           
+          </div>
+
+          {/* Fourth Downpara */}
+
+          <div className='contentparaF' onClick={toggleDropdown13}>
+            <p className='drop-paraF'>I live outside of India, so how can I purchase the course?</p>
+            <img
+              className={`dropdown-imgF ${isDropdownVisible13 ? 'rotate-180' : ''}`}
+              src="https://i.ibb.co/z8d8pyM/down-arrow.png"
+              alt=""
+            />
+          </div>
+
+          <div className={`downparaF ${isDropdownVisible13 ? 'show' : ''}`}>
+        
+            <p className='downnF'>The feature for international payments will be enabled by 22nd December.</p>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FAQs
