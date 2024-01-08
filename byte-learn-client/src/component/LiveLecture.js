39035a9ad3d0af2fbe7ccd91f6657e9cd93b9728
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2024-01-08 13:56:44 +0530

    Updated css

--- byte-learn-client/src/component/LiveLecture.js
+++ byte-learn-client/src/component/LiveLecture.js
@@ -6,20 +6,20 @@ const LiveLecture = () => {
     return (
         <>
           <header>
-            <h1><span className="recorded" style={{color:"Red"}}>Live </span> <span className="courses" style={{color:"Red"}}>Lectures</span></h1>
+            <h1><span className="recorded" >Live </span> <span className="courses"  style={{color:"var(--main-color)"}}>Lectures</span></h1>
           </header>
     
           <div className="containers">
             <div className="course">
               <img src="https://miro.medium.com/v2/resize:fit:1200/1*-EFdnPuVrwUOmYte11v0OA.png" alt="Course 1 Image" />
-              <div className="course-info" style={{display:"flex",justifyContent:"center",alignItems:"center",color:"red",fontWeight:"bolder"}}>
+              <div className="course-info" style={{display:"flex",justifyContent:"center",alignItems:"center",color:"gray",fontWeight:"bolder"}}>
                  <span>GO LIVE</span>
               </div>
             </div>
           <Link to="/attending">
           <div className="course">
               <img src="https://www.creativeitinstitute.com/images/course/course_1663052056.jpg" alt="Course 2 Image" />
-              <div className="course-info" style={{display:"flex",justifyContent:"center",alignItems:"center",color:"red",fontWeight:"bolder"}}>
+              <div className="course-info" style={{display:"flex",justifyContent:"center",alignItems:"center",color:"gray",fontWeight:"bolder"}}>
                  <span>GO LIVE</span>
               </div>
             </div>
